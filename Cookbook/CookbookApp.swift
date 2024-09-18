b2c4358e6924b56974a98ef976abdb1e782213b0
Chandler Griffin <chandlergriffin@chandlers-mbp-2.lan>
2024-09-18 09:41:18 -0400

    set up xcode project

---
+++ Cookbook/CookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookbookApp.swift
+//  Cookbook
+//
+//  Created by Chandler Griffin on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
